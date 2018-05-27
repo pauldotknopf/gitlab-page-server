77b7871cbc9df2a1ade3de838e87d5c23d48585e
Paul Knopf <pauldotknopf@gmail.com>
2018-05-26 23:29:51 -0400

    Added serilog.

---
+++ src/GitLabPages.Web/LoggingOptions.cs
@@ -0,0 +1,14 @@
+﻿using Serilog.Events;
+
+namespace GitLabPages.Web
+{
+    public class LoggingOptions
+    {
+        public LoggingOptions()
+        {
+            MinimumLevel = LogEventLevel.Information;
+        }
+        
+        public LogEventLevel MinimumLevel { get; set; }
+    }
+}
--- src/GitLabPages.Web/Program.cs
+++ src/GitLabPages.Web/Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace GitLabPages.Web
 {
@@ -17,8 +19,38 @@ namespace GitLabPages.Web
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        static IConfiguration CreateConfiguration()
+        {
+            using (var tmpHost = WebHost.CreateDefaultBuilder()
+                .ConfigureAppConfiguration((_, c) => { c.AddJsonFile("config.json", true); })
+                .Configure(app => { })
+                .Build())
+            {
+                return tmpHost.Services.GetRequiredService<IConfiguration>();
+            }
+        }
+
+        static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var configuration = CreateConfiguration();
+
+            var loggingOptions = new LoggingOptions();
+            var section = configuration.GetSection("Logging");
+            section.Bind(loggingOptions);
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Is(loggingOptions.MinimumLevel)
+                .WriteTo.Console()
+                .CreateLogger();
+            
+            return WebHost.CreateDefaultBuilder(args)
+                .UseConfiguration(configuration)
+                .ConfigureLogging(loggingBuilder =>
+                {
+                    loggingBuilder.ClearProviders();
+                    loggingBuilder.AddSerilog(dispose: true);
+                })
                 .UseStartup<Startup>();
+        }
     }
 }
